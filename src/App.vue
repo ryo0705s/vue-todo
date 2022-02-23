e55a230884d6121f79e1a84a909baba1108ff65f
ryo0705s <inspire.sasaki15@gmail.com>
2022-02-23 19:08:24 +0900

    todoの最低限の動作を確認

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>todoアプリ</h1>
+    <input v-model="todo" />
+    <button @click="add">追加</button>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        {{ todo }}<button @click="remove">X</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todo: "",
+      todos: [],
+    };
+  },
+
+  methods: {
+    add() {
+      this.todos.push(this.todo);
+      this.todo = "";
+    },
+    remove(index) {
+      this.todos.splice(index, 1);
+    },
+  },
+};
+</script>
